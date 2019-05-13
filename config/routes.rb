6820f2a96877de06d462f0fded22d6890bf42ed9
Ana Ghidelli <ana.ghidelli@gmail.com>
2019-05-13 16:40:25 +0100

    finished raking it, missing styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace :admin do
+  resources :restaurants do# , only: [:create, :index, :destroy]
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# t.string :name
+# t.string :address
+# t.string :phone_number
+# t.string :category
+# t.integer :stars
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'PlaceX',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '+44 123 123 123',
+    category:     'japanese',
+    stars:         5
+  },
+  {
+    name:         'PlaceY',
+    address:      '107A Middleton Road, London E2 7JE',
+    phone_number: '+44 123 123 123',
+    category:     'chinese',
+    stars:         5
+  },
+  {
+    name:         'PlaceZ',
+    address:      '14 Samuel Close, London E8 4SG',
+    phone_number: '+44 123 123 123',
+    category:     'italian',
+    stars:         5
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
